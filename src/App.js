944907343aa3bc77bd598143d5859885cde84434
Christiaan ten Voorde <CI.TenVoorde@student.han.nl>
2017-03-18 14:55:35 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -4,49 +4,6 @@ import './App.css';
 import {Link} from 'react-router'
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            ingelogdeUser: [undefined],
-            users: ['Bert', 'Ernie', 'Sander', 'Fritz'],
-            posts: [{
-                title: "post",
-                message: "Er zit een banaan in mijn oor!",
-                author: "Ernie",
-                time: new Date(100000000),
-                comments: [{
-                    message: "Nee Ernie, dat ben ik.",
-                    author: "Bert",
-                    time: new Date(300000000)
-                }],
-                likes: ['Ernie', 'Fritz']
-            }, {
-                title: "post",
-                message: "UNICORNS!",
-                author: "Fritz",
-                time: new Date(200000000),
-                comments: [],
-                likes: ['Ernie']
-            }, {
-                title: "post",
-                message: "Ghi Ghi Ghi!",
-                author: "Ernie",
-                time: new Date(400000000),
-                comments: [{
-                    message: "Hehehe",
-                    author: "Bert",
-                    time: new Date(500000000)
-                }, {
-                    message: "Hahaha",
-                    author: "Sander",
-                    time: new Date(600000000)
-                }],
-                likes: []
-            }]
-        }
-    }
-
     render() {
         return (
             <div className="App">
